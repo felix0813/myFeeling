a72e0912b23578ba31f30dee6f76da728abeca2f
felix <1306332027@qq.com>
2021-08-22 22:04:24 +0800

    "完善增加诗词的方法"

---
+++ my_feeling/lib/poem.dart
@@ -0,0 +1,14 @@
+class Poem{
+  var title;
+  var content;
+  var group;
+  var datetime;
+  var id;
+  Poem(int Id,String Title,String Group,String Datetime,String Content){
+    title=Title;
+    content=Content;
+    datetime=Datetime;
+    group=Group;
+    id=Id;
+  }
+}
