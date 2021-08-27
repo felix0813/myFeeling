9938fbe6e5e43057bcd53c54adbc73ecd60d3b68
felix <1306332027@qq.com>
2021-08-27 21:59:53 +0800

    "开始设置界面"

--- my_feeling/lib/pages/othersPage.dart
+++ my_feeling/lib/pages/othersPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mysql1/mysql1.dart' as mysql;
 
 class Others extends StatefulWidget {
   @override
@@ -8,6 +9,24 @@ class Others extends StatefulWidget {
 }
 
 class OthersState extends State<Others> {
+  //连接数据库&查询
+  Future connectDatabase() async{
+    var settings = new mysql.ConnectionSettings(
+        host: 'rm-wz903m77zaza3173jmo.mysql.rds.aliyuncs.com',
+        port: 3306,
+        user: 'felix',
+        password: 'wzf_0813',
+        db: 'my_db'
+    );
+    var conn = await mysql.MySqlConnection.connect(settings);
+    //var userId = yxcode;
+    var results = await conn.query('select count(*) as cnt from test');
+    await conn.close();
+    print('$results');
+    print(results);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +35,33 @@ class OthersState extends State<Others> {
           backgroundColor: Color.fromRGBO(0, 255, 255, 0.5),
           title: Text("其他设置"),
         ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                //TO DO
+              ),
+              FutureBuilder(
+          future: connectDatabase(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Text('Press button to start.');
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return Center(
+                    child: Text(
+                      'Awaiting result...',
+                      style: TextStyle(fontSize: 36),
+                    ));
+              case ConnectionState.done:
+                return Center(
+                    child: Text(
+                      'Got result...',
+                      style: TextStyle(fontSize: 36),
+                    ));
+            }
+          }),])
       ),
     );
   }
